b8ff6a36286225d75b25b494e98f8aa2e9fda77b
youyui <976124940@qq.com>
2018-11-29 21:55:44 +0800

    【nagirpc】相关初始化

---
+++ nagi-codec/src/main/java/com/nagi/codec/bean/Dog.java
@@ -0,0 +1,48 @@
+package com.nagi.codec.bean;
+
+import java.util.Map;
+
+public class Dog {
+    private String name;
+    private int age;
+    private Map<String,Object> attributes;
+
+    public Dog(String name, int age, Map<String, Object> attributes) {
+        this.name = name;
+        this.age = age;
+        this.attributes = attributes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", attributes=" + attributes +
+                '}';
+    }
+}
